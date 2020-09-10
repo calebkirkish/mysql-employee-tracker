c1fd322eeb4c9f2322897cf1a9aaead894ca8411
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-09-10 16:15:51 -0700

    updated the database table columns & added some data to play around with in seed

--- db/schema.sql
+++ db/schema.sql
@@ -4,22 +4,44 @@ CREATE DATABASE starwars_db;
 
 USE starwars_db;
 
+CREATE TABLE allegiance (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(20),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    role_name VARCHAR(30),
+    midicount INT(10),
+    allegiance_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (allegiance_id)
+)
+
 CREATE TABLE characters (
     id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
+    allegiance_id,
     is_jedi BOOLEAN NOT NULL,
-    role_id 
-    PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+    role_id INT,
+    leader_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (leader_id) REFERENCES characters(id)
 )
 
-SELECT * FROM characters WHERE is_jedi=1
+INSERT INTO allegiance(name)
+VALUES 
+    ('dark side'),
+    ('light side')
 
-INSERT INTO characters (name, is_jedi)
-VALUES ('Luke Skywalker', 1)
-INSERT INTO characters (name, is_jedi)
-VALUES ('Han Solo', 0)
-INSERT INTO characters (name, is_jedi)
-VALUES ('Leia Organa-Solo', 1)
-INSERT INTO characters (name, is_jedi)
-VALUES ('Baby Yoda', 1)
+INSERT into role 
+    (role_name, midicount, allegiance_id)
+
+VALUES
+    ('Jedi Knight', 15000, 2)
+
+INSERT INTO characters (name, allegiance_id, role_id, leader_id,is_jedi)
+VALUES ('Luke Skywalker', 2, 1, 1)
+
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO characters (name, is_jedi)
+VALUES ('Luke Skywalker', 1)
+INSERT INTO characters (name, is_jedi)
+VALUES ('Han Solo', 0)
+INSERT INTO characters (name, is_jedi)
+VALUES ('Leia Organa-Solo', 1)
+INSERT INTO characters (name, is_jedi)
+VALUES ('Baby Yoda', 1)
